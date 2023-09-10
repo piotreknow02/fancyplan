d95479efbed5d991206d2399cd5dccb5b040bd37
Piotr Nowakowski <piotrek02nowakowski@gmail.com>
2023-09-10 21:01:03 +0200

    redis cache

---
+++ src/cache.rs
@@ -0,0 +1,26 @@
+use redis::{Client, Connection, cmd, Commands};
+use anyhow::{Result, Ok};
+
+const CACHE_LIFETIME: usize = 3600; // seconds
+
+pub struct CachePrivoder {
+    connection: Connection,
+} 
+
+impl CachePrivoder {
+    pub fn from_dsn(dsn: &str) -> Result<Self> {
+        let client = Client::open(dsn)?;
+        let conn = client.get_connection()?;
+        Ok(Self { connection: conn })
+    }
+
+    pub fn get(&mut self, url: &str) -> Option<String> {
+        let res: Option<String> = self.connection.get(url).unwrap_or(None);
+        res
+    }
+
+    pub fn set(&mut self, url: &str, content: &str) -> Result<()> {
+        let _ = self.connection.set_ex(url, content, CACHE_LIFETIME)?;
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,22 @@ mod upgrader;
 mod scraper;
 mod cache;
 
-use actix_web::{HttpServer, App};
+use actix_web::{HttpServer, App, middleware, web::Data};
+use cache::CachePrivoder;
 use server::{index, plan};
+
+const REDIS_URL: &str = "redis://redis/";
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let cache: Option<CachePrivoder> = match CachePrivoder::from_dsn(REDIS_URL) {
+        Ok(v) => Some(v),
+        Err(e) => None,
+    };
+
     HttpServer::new(|| {
         App::new()
+            .wrap(middleware::Logger::default())
             .service(plan)
             .service(index)
     })
